5f6970d16df15998649193aa306b1ff2c8b20754
SlyJackal <slyjackal4@gmail.com>
2020-07-16 22:29:38 +0500

    Update main.py
    if

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def word_in_text(message: types.Message):
     text = message.text.split()
     if {'Вера', 'вера', 'Писька', 'писька', 'Писька!', 'писька!', 'Веры', 'веры'} & set(text):
         await message.answer('Писька!')
-    elif {'Вова', 'Никита', 'Игорь', 'Наташа', 'Алексей'} & set(text):
+    if {'Вова', 'Никита', 'Игорь', 'Наташа', 'Алексей'} & set(text):
         await message.answer('Я не буду писать это слово!')
 
 
