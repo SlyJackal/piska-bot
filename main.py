35fced76560c4282b7179b9b3078af3e57b3c09d
SlyJackal <slyjackal4@gmail.com>
2020-07-16 22:33:28 +0500

    Update main.py
    new triggers

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ async def word_in_text(message: types.Message):
     text = message.text.split()
     if {'Вера', 'вера', 'Писька', 'писька', 'Писька!', 'писька!', 'Веры', 'веры'} & set(text):
         await message.answer('Писька!')
-    if {'Вова', 'Никита', 'Игорь', 'Наташа', 'Алексей'} & set(text):
+    if {'Вова', 'Никита', 'Игорь', 'Алексей'} & set(text):
         await message.answer('Я не буду писать это слово!')
-
+    if {'Игорь'} & set(text):
+        await message.answer('Жрать')
+    if {'Наташа'} & set(text):
+        await message.answer('Бежать')
 
 
 if __name__ == '__main__':
