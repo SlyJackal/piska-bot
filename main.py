84d9ff99bae70af4ceb095551326c42ce91a830b
SlyJackal <slyjackal4@gmail.com>
2020-07-16 22:48:05 +0500

    Update main.py
    more words

--- main.py
+++ main.py
@@ -15,18 +15,20 @@ async def start(message: types.Message):
 @dp.message_handler()
 async def word_in_text(message: types.Message):
     text = message.text.split()
-    if {'Вера', 'вера', 'Писька', 'писька', 'Писька!', 'писька!', 'Веры', 'веры'} & set(text):
+    if {'Вера', 'вера', 'Писька', 'писька', 'Писька!', 'писька!', 'Веры', 'веры', 'Вера?'} & set(text):
         await message.answer('Писька!')
-    if {'Вова'} & set(text):
+    if {'Вова', 'Вова?', 'Вова!'} & set(text):
         await message.answer('Я не буду писать это слово!')
-    if {'Никита'} & set(text):
+    if {'Никита', 'Никита?', 'Никита!'} & set(text):
         await message.answer('Котлетка!')
-    if {'Игорь', 'Иииииигооорь Котов'} & set(text):
+    if {'Игорь', 'Игорь?', 'Игорь!'} & set(text):
         await message.answer('Жрать!')
-    if {'Наташа'} & set(text):
+    if {'Наташа', 'Наташа?', 'Наташа!'} & set(text):
         await message.answer('Бежать!')
-    if {'Алексей'} & set(text):
+    if {'Алексей', 'Алексей?', 'Алексей!'} & set(text):
         await message.answer('Папка!')
+    if {'регресс'} & set(text):
+        await message.answer('ПИСЬКА! ПИСЬКА! ПИСЬКА!')
 
 
 if __name__ == '__main__':
