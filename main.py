066798d9fe1e4a42e3607fd28e2a407fddc93b2a
SlyJackal <slyjackal4@gmail.com>
2020-07-18 16:51:08 +0500

    Update main.py
    bug fix

--- main.py
+++ main.py
@@ -35,27 +35,27 @@ async def start(message: types.Message):
 async def word_in_text(message: types.Message):
     if re.search(r'\b(?:вера|писька)\b', message.text.lower()) is not None:
         await message.answer('Писька!')
-    if re.search(r'\b(?:Вова|Владимир)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:вова|владимир)\b', message.text.lower()) is not None:
         await message.answer('Он молодец :P')
-    if re.search(r'\b(?:Никита|Никитка|Ник)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:никита|никитка|Ник)\b', message.text.lower()) is not None:
         await message.answer('Котлетка!')
-    if re.search(r'\b(?:Игорь|Котов|Иииииигооорь)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:Игорь|Котов|иииииигооорь)\b', message.text.lower()) is not None:
         await message.answer('Жрать!')
-    if re.search(r'\b(?:Наташа|Нат|Наташка)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:наташа|нат|наташка)\b', message.text.lower()) is not None:
         await message.answer('Бежать!')
-    if re.search(r'\b(?:Алексей|Леша|Лёха|Лёша|Леха)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:алексей|леша|лёха|лёша|леха)\b', message.text.lower()) is not None:
         await message.answer('Папка!')
-    if re.search(r'\b(?:Регресс|Срочно|надо)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:регресс|срочно|надо)\b', message.text.lower()) is not None:
         await message.answer('ПИСЬКА! ПИСЬКА! ПИСЬКА!')
-    if re.search(r'\b(?:Загоруйк|Виталя)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:загоруйк|виталя)\b', message.text.lower()) is not None:
         await message.answer('Всякий!')
-    if re.search(r'\b(?:Антон|Старовойтов)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:антон|старовойтов)\b', message.text.lower()) is not None:
         await message.answer('Сила!')
     if re.search(r'\b(?:сук|блять|пиздец|хуй|нах)\b', message.text.lower()) is not None:
         await message.answer('Не матерись!')
     if re.search(r'\b(?:попа|попка)\b', message.text.lower()) is not None:
         await message.answer('Я люблю попки жено-ботов!')
-    if re.search(r'\b(?:Спасибо)\b', message.text.lower()) is not None:
+    if re.search(r'\b(?:спасибо)\b', message.text.lower()) is not None:
         await message.answer('Рад служить!')
 
 if __name__ == '__main__':
