41091eccf966831c278dd68016d8205178d6991e
SlyJackal <slyjackal4@gmail.com>
2021-06-28 12:47:17 +0500

    Clean_1_main.py

--- main.py
+++ main.py
@@ -12,25 +12,6 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     await message.answer('Привет!')
 
-
-#@dp.message_handler()
-#async def word_in_text(message: types.Message):
- #   text = message.text.split()
-  #  if {'Вера', 'вера', 'Писька', 'писька', 'Писька!', 'писька!', 'Веры', 'веры', 'Вера?'} & set(text):
-   #     await message.answer('Писька!')
-    #if {'Вова', 'Вова?', 'Вова!'} & set(text):
-     #   await message.answer('Я не буду писать это слово!')
-    #if {'Никита', 'Никита?', 'Никита!'} & set(text):
-        #await message.answer('Котлетка!')
-    #if {'Игорь', 'Игорь?', 'Игорь!'} & set(text):
-    #    await message.answer('Жрать!')
-    #if {'Наташа', 'Наташа?', 'Наташа!'} & set(text):
-    #    await message.answer('Бежать!')
-    #if {'Алексей', 'Алексей?', 'Алексей!'} & set(text):
-    #    await message.answer('Папка!')
-    #if {'регресс'} & set(text):
-    #    await message.answer('ПИСЬКА! ПИСЬКА! ПИСЬКА!')
-
 @dp.message_handler()
 async def word_in_text(message: types.Message):
     if re.search(r'\b(?:вера|писька|ошибка|баг|ошибся|ошиблась|bug)\b', message.text.lower()) is not None:
